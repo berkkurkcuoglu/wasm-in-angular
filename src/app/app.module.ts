a84e26a63cbf445cf9325e75b152a5255618f0b8
Berk Kurkcuoglu <bkurkcu1@binghamton.edu>
2019-11-11 21:17:51 +0300

    Fibonacci demo ready.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FibonacciComponent } from './components/fibonacci/fibonacci.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FibonacciComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, FibonacciComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/fibonacci/fibonacci.component.ts
+++ src/app/components/fibonacci/fibonacci.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { filter, tap } from 'rxjs/operators';
 
 import { FibonacciWasmService } from './../../services/fibonacci-wasm.service';
 
@@ -8,16 +10,34 @@ import { FibonacciWasmService } from './../../services/fibonacci-wasm.service';
   styleUrls: ['./fibonacci.component.scss'],
   providers: [FibonacciWasmService]
 })
-export class FibonacciComponent implements OnInit {
-  jsResult: any;
-  wasmResult: any;
+export class FibonacciComponent implements OnInit, OnDestroy {
+  public wasmReady: boolean = false;
+  public fiboNum: number;
+  public result: number;
+  public elapsedTime: number;
+  public calculating: boolean = false;
+
+  private subscription: Subscription = new Subscription();
 
   constructor(private wasmService: FibonacciWasmService) {}
 
   ngOnInit() {
-    const num = 40;
-    this.wasmResult = this.wasmService.fibonacci(num);
-    this.jsResult = this.fibonacci(num);
+    this.checkWasmStatus();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  private checkWasmStatus(): void {
+    this.subscription.add(
+      this.wasmService.wasmReady$
+        .pipe(
+          filter(isReady => isReady),
+          tap(isReady => (this.wasmReady = isReady))
+        )
+        .subscribe()
+    );
   }
 
   private fibonacci(n: number): number {
@@ -25,5 +45,25 @@ export class FibonacciComponent implements OnInit {
       return n;
     }
     return this.fibonacci(n - 1) + this.fibonacci(n - 2);
+  }
+
+  public calculate(type: string): void {
+    if (!this.calculating) {
+      this.calculating = true;
+      this.elapsedTime = 0;
+      this.result = undefined;
+      setTimeout(() => {
+        const start = new Date();
+        if (type === 'WASM') {
+          this.result = this.wasmService.fibonacci(this.fiboNum);
+        }
+        if (type === 'JS') {
+          this.result = this.fibonacci(this.fiboNum);
+        }
+        const end = new Date();
+        this.elapsedTime = end.getTime() - start.getTime();
+        this.calculating = false;
+      }, 100);
+    }
   }
 }
